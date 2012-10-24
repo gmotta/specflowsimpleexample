34e4c02f3a7ddb9ab7c3807b1cecbb032dd27e2d
Motta, Guilherme <gfcmotta@gmail.com>
2012-10-24 14:11:01 -0300

    sharing the empty solution with a sample feature and no steps implemented

---
+++ specflowsimpleexample/Features/SimpleSearch.feature
@@ -0,0 +1,8 @@
+﻿Feature:
+	Basic Search
+
+Scenario:  Search For 'Eloquent Ruby'
+    Given I am on the Bing Home Page
+    When I search for "Eloquent Ruby"
+    And I follow the "Eloquent Ruby" link
+    Then I should see the title "Eloquent Ruby | Companion site to the book Eloquent Ruby"
---
+++ specflowsimpleexample/StepDefinitions/SimpleSearchStepDefinition.cs
@@ -0,0 +1,33 @@
+﻿using TechTalk.SpecFlow;
+
+namespace specflowspike.StepDefinitions
+{
+    [Binding]
+    public class SimpleSearchStepDefinition
+    {
+        [Given(@"I am on the Bing Home Page")]
+        public void GivenIAmOnTheBingHomePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I search for ""Eloquent Ruby""")]
+        public void WhenISearchForEloquentRuby()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I follow the ""Eloquent Ruby"" link")]
+        public void WhenIFollowTheEloquentRubyLink()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I should see the title ""Eloquent Ruby \| Companion site to the book Eloquent Ruby""")]
+        public void ThenIShouldSeeTheTitleEloquentRubyCompanionSiteToTheBookEloquentRuby()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
